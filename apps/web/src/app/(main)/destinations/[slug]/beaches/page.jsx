1b556131f8cb64f92aec1205b5f1b60b104e1b41
Victor12-star <okonv09@gmail.com>
2026-09-04 21:14:53 +0200

    feat(web): wire destination beaches route

--- apps/web/src/app/(main)/destinations/[slug]/beaches/page.jsx
+++ apps/web/src/app/(main)/destinations/[slug]/beaches/page.jsx
@@ -1,1 +1,32 @@
-export { default } from '@/components/common/unavailable-page.jsx';
+import { getCountryDisplayName } from '@attravoya/localization';
+
+import { BeachesPage } from '../../../../../features/destinations/beaches-page.jsx';
+import { parseDestinationSelection } from '../../../../../features/destinations/destination-route.js';
+import { loadMessages } from '../../../../../i18n/messages.js';
+import { getRequestLocale } from '../../../../../i18n/request-locale.js';
+
+export default async function DestinationBeachesRoutePage({ params, searchParams }) {
+  const [{ slug }, resolvedSearchParams, locale] = await Promise.all([
+    params,
+    searchParams,
+    getRequestLocale(),
+  ]);
+  const messages = await loadMessages(locale);
+  const destination = parseDestinationSelection({ slug, searchParams: resolvedSearchParams });
+
+  return (
+    <BeachesPage
+      destination={
+        destination
+          ? {
+              ...destination,
+              countryDisplayName:
+                getCountryDisplayName(destination.countryCode, locale) ?? destination.countryCode,
+            }
+          : null
+      }
+      locale={locale}
+      messages={messages}
+    />
+  );
+}
