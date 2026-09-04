eb5b34eb9ecf8bc7fbc502d148b351b1ea8f561d
Victor12-star <okonv09@gmail.com>
2026-09-04 17:18:54 +0200

    feat: add dedicated destinations search page

--- apps/web/src/app/(main)/destinations/page.jsx
+++ apps/web/src/app/(main)/destinations/page.jsx
@@ -1,1 +1,10 @@
-export { default } from '@/components/common/unavailable-page.jsx';
+import { DestinationSearch } from '../../../features/destinations/destination-search.jsx';
+import { getRequestLocale } from '../../../i18n/request-locale.js';
+import { loadMessages } from '../../../i18n/messages.js';
+
+export default async function DestinationsPage() {
+  const locale = await getRequestLocale();
+  const messages = await loadMessages(locale);
+
+  return <DestinationSearch locale={locale} messages={messages} />;
+}
