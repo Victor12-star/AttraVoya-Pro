9e2b6c1b1d7d50bbcb6b553491bd71da9f7f9d4a
Victor12-star <okonv09@gmail.com>
2026-09-05 08:56:58 +0200

    feat(web): add destination transport route

---
+++ apps/web/src/app/(main)/destinations/[slug]/transport/page.jsx
@@ -0,0 +1,32 @@
+import { getCountryDisplayName } from '@attravoya/localization';
+
+import { parseDestinationSelection } from '../../../../../features/destinations/destination-route.js';
+import { TransportDestinationPage } from '../../../../../features/destinations/transport-page.jsx';
+import { loadMessages } from '../../../../../i18n/messages.js';
+import { getRequestLocale } from '../../../../../i18n/request-locale.js';
+
+export default async function DestinationTransportRoutePage({ params, searchParams }) {
+  const [{ slug }, resolvedSearchParams, locale] = await Promise.all([
+    params,
+    searchParams,
+    getRequestLocale(),
+  ]);
+  const messages = await loadMessages(locale);
+  const destination = parseDestinationSelection({ slug, searchParams: resolvedSearchParams });
+
+  return (
+    <TransportDestinationPage
+      destination={
+        destination
+          ? {
+              ...destination,
+              countryDisplayName:
+                getCountryDisplayName(destination.countryCode, locale) ?? destination.countryCode,
+            }
+          : null
+      }
+      locale={locale}
+      messages={messages}
+    />
+  );
+}
