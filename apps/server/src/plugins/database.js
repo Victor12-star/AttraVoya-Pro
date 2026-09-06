d8a1dd9b6a9cf71c10d9bc5b3581c50d2290b4e9
Victor12-star <okonv09@gmail.com>
2026-09-06 23:03:40 +0200

    Phase 8R: narrow database lifecycle contract

--- apps/server/src/plugins/database.js
+++ apps/server/src/plugins/database.js
@@ -1,8 +1,13 @@
 import { prisma } from '@attravoya/database';
 
+/** @typedef {{ $disconnect: () => Promise<void> }} DatabaseLifecycleClient */
+
 /**
  * Tie the process-wide Prisma client to Fastify's lifecycle so rolling deploys
  * and replica termination explicitly release PostgreSQL pool connections.
+ *
+ * @param {import('fastify').FastifyInstance} app
+ * @param {{ databaseClient?: DatabaseLifecycleClient }} [options]
  */
 export function registerDatabaseLifecycle(app, { databaseClient = prisma } = {}) {
   app.addHook('onClose', async () => {
