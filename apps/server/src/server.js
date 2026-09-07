6c7cf4045c57741e86bb50ef7fcc3d4038834169
Victor12-star <okonv09@gmail.com>
2026-09-07 16:05:39 +0200

    Share readiness state with shutdown handler

--- apps/server/src/server.js
+++ apps/server/src/server.js
@@ -1,12 +1,14 @@
 import { env } from './config/env.js';
 import { buildApp } from './app.js';
+import { createReadinessState } from './lifecycle/readiness-state.js';
 import { registerDatabaseLifecycle } from './plugins/database.js';
 import { createShutdownHandler } from './shutdown.js';
 
-const app = await buildApp();
+const readinessState = createReadinessState();
+const app = await buildApp({ readinessState });
 registerDatabaseLifecycle(app);
 
-const shutdown = createShutdownHandler({ app });
+const shutdown = createShutdownHandler({ app, readinessState });
 
 for (const signal of ['SIGINT', 'SIGTERM']) {
   process.once(signal, () => {
