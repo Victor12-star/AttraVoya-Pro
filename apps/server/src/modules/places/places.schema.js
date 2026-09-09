a7dc0e2b3f6b11d9cd6ca438d93413e055051410
Victor12-star <okonv09@gmail.com>
2026-09-09 13:26:50 +0200

    Phase 9U: expose consular query schema

--- apps/server/src/modules/places/places.schema.js
+++ apps/server/src/modules/places/places.schema.js
@@ -1,6 +1,11 @@
-import { placesAutocompleteQuerySchema, placesNearbyQuerySchema } from '@attravoya/validation';
+import {
+  consularMissionQuerySchema,
+  placesAutocompleteQuerySchema,
+  placesNearbyQuerySchema,
+} from '@attravoya/validation';
 
 export const placesSchemas = Object.freeze({
   autocomplete: { querystring: placesAutocompleteQuerySchema },
   nearby: { querystring: placesNearbyQuerySchema },
+  consularMissions: { querystring: consularMissionQuerySchema },
 });
