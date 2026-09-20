5d6d9ecde77e81592084c1dc69d3a3f480c183b3
Victor12-star <okonv09@gmail.com>
2026-09-20 13:02:01 +0200

    Phase 10BF: export adaptive design contracts

--- packages/design-tokens/src/index.js
+++ packages/design-tokens/src/index.js
@@ -5,3 +5,11 @@ export { radius } from './radius.js';
 export { shadows } from './shadows.js';
 export { motion } from './motion.js';
 export { darkTheme, lightTheme, themes } from './themes.js';
+export {
+  contentWidths,
+  getPageGutter,
+  getWindowSizeClass,
+  pageGutters,
+  windowBreakpoints,
+} from './layout.js';
+export { feedbackTiming, interaction } from './interaction.js';
