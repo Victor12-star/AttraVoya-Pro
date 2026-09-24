6f41e5444740021729f01bae643391007f8fe802
Victor12-star <okonv09@gmail.com>
2026-09-24 20:40:53 +0200

    style: wrap RevenueCat identity test import

--- apps/server/src/modules/payments/payments.revenuecat-subscriber-identity.test.js
+++ apps/server/src/modules/payments/payments.revenuecat-subscriber-identity.test.js
@@ -1,7 +1,9 @@
 import { describe, expect, it, vi } from 'vitest';
 
 import { createPaymentsRepository } from './payments.repository.js';
-import { createRevenueCatSubscriberIdentityService } from './payments.revenuecat-subscriber-identity.js';
+import {
+  createRevenueCatSubscriberIdentityService,
+} from './payments.revenuecat-subscriber-identity.js';
 
 function identity(overrides = {}) {
   return {
