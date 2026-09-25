4f5a38e93db8b34c09da720ff5ffd4c4d5deed26
Victor12-star <okonv09@gmail.com>
2026-09-25 19:52:54 +0200

    fix: tighten RevenueCat Android public key format

--- apps/mobile/tooling/revenuecat-public-config.js
+++ apps/mobile/tooling/revenuecat-public-config.js
@@ -1,6 +1,6 @@
 import process from 'node:process';
 
-const REVENUECAT_ANDROID_PUBLIC_KEY_PATTERN = /^goog_[A-Za-z0-9_-]{4,195}$/;
+const REVENUECAT_ANDROID_PUBLIC_KEY_PATTERN = /^goog_[A-Za-z0-9]{4,195}$/;
 
 function configurationError(message) {
   return new Error(`RevenueCat Android configuration: ${message}`);
